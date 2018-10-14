e8bb6527acbc177f66f4e23b9a3e698d42a3a4fa
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2018-10-14 13:14:28 +0100

    Update proposal name

--- src/app/Home.js
+++ src/app/Home.js
@@ -301,7 +301,7 @@ class Home extends Component {
         <p>
           Statement of intent:
         </p>
-        {statement}
+        {this.fromHex(statement)}
         <p>
           List of members:
         </p>
@@ -341,7 +341,7 @@ class Home extends Component {
         {proposals
           .map((obj, index) => (
             <ul key={index}>
-              <li>Proposal name: {web3.toAscii(obj[0])}</li>
+              <li>Proposal name: {obj[0]}</li>
               <li>recipient: {obj[3].toString()}</li>
               <li>value: {obj[4].toNumber()}</li>
               <li>data: {'' + web3.toAscii(obj[5])}</li>
