65c540ec506a639b789daad772e9462ee6677378
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2019-04-15 21:38:41 +0100

    Fix issues and changes

--- src/app/app.js
+++ src/app/app.js
@@ -1,24 +1,27 @@
-import React from 'react'
-import { render } from 'react-dom'
-import injectTapEventPlugin from 'react-tap-event-plugin'
+import React from "react";
+import { render } from "react-dom";
+import injectTapEventPlugin from "react-tap-event-plugin";
 // import { Router, Route, hashHistory } from 'react-router'
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import getMuiTheme from 'material-ui/styles/getMuiTheme'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import Home from './Home'
-import '../www/styles/main.scss'
+import getMuiTheme from "material-ui/styles/getMuiTheme";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import Home from "./Home";
+import "../www/styles/main.scss";
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
-
-render((
-    <MuiThemeProvider muiTheme={getMuiTheme()}>
-        <Router>
-            <Route path="/" render={({ location, history }) => {
-                return <Home history={history} />
-            }} />
-        </Router>
-    </MuiThemeProvider>
-), document.getElementById('app'))
+render(
+  <MuiThemeProvider muiTheme={getMuiTheme()}>
+    <Router>
+      <Route
+        path="/"
+        render={({ location, history }) => {
+          return <Home location={location} history={history} />;
+        }}
+      />
+    </Router>
+  </MuiThemeProvider>,
+  document.getElementById("app")
+);
